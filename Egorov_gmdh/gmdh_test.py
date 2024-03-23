2094312d8152f4c106f6711491de47d73ea4c279
EleottD <esergey236@gmail.com>
2024-03-23 10:08:55 +0300

    Model changed to Ria, added sequential criterion using

--- Egorov_gmdh/gmdh_test.py
+++ Egorov_gmdh/gmdh_test.py
@@ -8,20 +8,22 @@ import Essentials
 
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import RandomizedSearchCV
 import gmdh
-from gmdh import Mia, split_data
+from gmdh import Ria, split_data
+from gmdh import CriterionType, SequentialCriterion, Solver, PolynomialType
 
-# Загрузка данных. Возьмем для примера архив Data_Average
+# Загрузка данных. Возьмем архив Data_Average
 
-a=np.load('Data_Average.npz', allow_pickle=True)
+a=np.load('../Data_Average.npz', allow_pickle=True)
 
 # Загрузка и подготовка данных
 
-x1=a['X_test_3']
-x2=a['X_train_3']
+x1=a['X_test_2']
+x2=a['X_train_2']
 
-y1=a['Y_test_3']
-y2=a['Y_train_3']
+y1=a['Y_test_2']
+y2=a['Y_train_2']
 
 timestamp1=y1[:, 1]
 timestamp2=y2[:, 1]
@@ -33,12 +35,12 @@ y2 = y2.astype(np.float64)
 
 # Покажем, что данные действительно нужной размерности
 
-print(y1.shape, "\n")
-print(x1.shape, "\n")
+print(y2.shape, "\n")
+print(x2.shape, "\n")
 
 # Создание собственного класса с Виртуальным анализатором на основе шаблона SoftSensor из файла Essentials.py
 
-class gmdh_Mia(Essentials.SoftSensor):
+class gmdh_Ria(Essentials.SoftSensor):
     def __init__(self, x_train, y_train):
         super().__init__('Test')
         self.x_scaler = StandardScaler()
@@ -47,7 +49,7 @@ class gmdh_Mia(Essentials.SoftSensor):
         self.y_scaler.fit(y_train)
         self.train(x_train, y_train)
 
-    def prepocessing(self, x):
+    def preprocessing(self, x):
         try:
             return self.x_scaler.transform(x)
         except:
@@ -72,10 +74,28 @@ class gmdh_Mia(Essentials.SoftSensor):
         return predictions.reshape(-1, 1)
 
     def train(self, x_train, y_train):
-        preproc_y = self.prepocessing(y_train)
-        preproc_x = self.prepocessing(x_train)
-        model = gmdh.Mia()
-        model.fit(preproc_x, preproc_y, k_best=10, test_size=0.26)
+        preproc_y = self.preprocessing(y_train)
+        preproc_x = self.preprocessing(x_train)
+        seq_criterion=SequentialCriterion(criterion_type=CriterionType.SYM_REGULARITY, second_criterion_type=CriterionType.SYM_ABSOLUTE_NOISE_IMMUNITY, solver=Solver.ACCURATE, top=2)
+        model = gmdh.Ria()
+        #help(seq_criterion)
+        #help(model.fit)
+
+        # Метод подбора параметров обучения. При изменении параметров k_best и test_size возможно получить адекватные показатели коэффициента детерминации для каждого набора данных
+
+        #param_dist = {
+        #    'k_best': range(3, 18, 1),
+        #    'test_size': [i/100 for i in range(20, 71)],
+        #    'limit': [0, 0.01, 0.02, 0.05, 0.1]
+        #}
+        #model_estimator = model.fit(preproc_x, preproc_y)
+        #random_search = RandomizedSearchCV(estimator=model_estimator, param_distributions=param_dist, n_iter=1000, cv=3,
+        #                                   n_jobs=-1, verbose=3, scoring='neg_mean_squared_error')
+        #random_search.fit(preproc_x, preproc_y)
+        #print(random_search.best_params_)
+
+
+        model.fit(preproc_x, preproc_y, k_best=10, test_size=0.55, n_jobs=-1, verbose=1, limit=0, p_average=2, criterion=seq_criterion, polynomial_type=PolynomialType.QUADRATIC)
         self.set_model(model)
 
     def __str__(self):
@@ -84,7 +104,7 @@ class gmdh_Mia(Essentials.SoftSensor):
 
 # Создание экземпляра класса с алгоритмом gmdh mia
 
-Test_sensor_1=gmdh_Mia(x2, y2)
+Test_sensor_1=gmdh_Ria(x2, y2)
 #print(x2)
 # Пример работы метода str
 
@@ -97,5 +117,5 @@ Test_sensor_1.test(x1, y1, metric)
 
 # Визуализация работы алгоритма
 
-test_visual=Essentials.Visualizer(x1, y1, timestamp1, [metric], 'Test gmdh Mia Sensor R2 metric')
+test_visual=Essentials.Visualizer(x1, y1, timestamp1, [metric], 'Test gmdh Ria Sensor R2 metric')
 test_visual.visualize([Test_sensor_1])
