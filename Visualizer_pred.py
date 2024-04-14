9580a8e6861f67258638444000d4443f350d8e05
EleottD <esergey236@gmail.com>
2024-04-14 22:31:27 +0300

    Added using graphics with numerous models, added legend for 2nd and 3rd graphics, changed vertical lines and other visual aspects

--- Visualizer_pred.py
+++ Visualizer_pred.py
@@ -74,105 +74,117 @@ class Visualizer():
             table = tabulate(data, headers='firstrow', tablefmt='grid')
             print(table)
             #print(df_metric)
-        results = {}
-        real=self.y_real
-        if real.ndim==2:
-            real=np.squeeze(real)
-        #print(real.shape)
-        #print(real.shape)
-        #print(real.shape)
-        results['From Lab'] = real
-        for model in models:
-            model_pred, _ = model.test(self.x_test, self.y_real, self.metrics[0])
-            results[model.get_name()] = np.squeeze(model_pred)
-        res = {}
-        res['From Lab'] = self.y_real
-        for model in models:
-            model_pred, _ = model.test(self.x_test, self.y_real, self.metrics[0])
-            res['Arrange']=np.squeeze(np.subtract(model_pred, res['From Lab']))
-
-        #fig1, ax1 = plt.subplots()
-        #fig2, ax2 = plt.subplots()
-        #fig3, ax3 = plt.subplots()
-        #fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(8, 10))
         fig = plt.figure(figsize=(10, 8))
-        grid = GridSpec(3, 1)
-        ax1 = fig.add_subplot(grid[0, 0])
-        ax2 = fig.add_subplot(grid[1, 0])
-        ax3 = fig.add_subplot(grid[2, 0])
-        for name, data in results.items():
-            #print(data.shape)
-            #print(type(data))
-            ax1.plot(self.timestamps, data, linestyle='', marker=".", markersize=8, label=name)
-            ax2.plot(res['From Lab'], res['Arrange'], linestyle='', marker='.')
-            ax3.plot(results['From Lab'], results[model.get_name()], linestyle='', marker='.')
-            #ax2.plot()
-            #print(data)
-        if lines:
-            for i in range(len(self.timestamps)-1):
-                ax1.plot([self.timestamps[i], self.timestamps[i+1]], [results['From Lab'][i], results['From Lab'][i+1]], color='blue',
-                         linestyle='-', linewidth=0.6)
-                ax1.plot([self.timestamps[i], self.timestamps[i + 1]],
-                         [results[model.get_name()][i], results[model.get_name()][i + 1]], color='orange',
-                         linestyle='-', linewidth=0.6)
-        if lines_vertical:
-            for i in range(len(self.timestamps)):
-                ax1.plot([self.timestamps[i], self.timestamps[i]], [results['From Lab'][i], results[model.get_name()][i]], color='gray',
-                         linestyle='-')
-        x=np.linspace(min(real), max(real), 100)
-        y=x
-        ax3.plot(x,y, linestyle='-', color='green', linewidth=0.6)
-        # Устанавливаем параметры визуализации
-        ax1.set_title(self.name)
-        ax1.set_xlabel('Date')
-        ax1.set_ylabel('Count', rotation=90)
-        ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
-        ax1.xaxis.set_minor_formatter(mdates.AutoDateFormatter('%Y-%m'))
-        ax1.xaxis.set_tick_params(rotation=30)
-        ax1.legend(loc='best')
+        grid = GridSpec(3, len(models))
 
-        ax2.set_title("Model prediction errors")
-        ax2.set_xlabel('Real parameters')
-        ax2.set_ylabel('Error', rotation=90)
-        #ax2.set_xticks(rotation=45)
+        for metric in self.metrics:
+            for i, model in enumerate(models):
 
-        ax3.set_title('Real-predicted relation')
-        ax3.set_xlabel('Real')
-        ax3.set_ylabel('Predicted', rotation=90)
-        ax3.xaxis.set_tick_params(rotation=0)
-        #ax3.legend(loc='best')
+                results = {}
+                real=self.y_real
+                if real.ndim==2:
+                    real=np.squeeze(real)
+                #print(real.shape)
+                #print(real.shape)
+                #print(real.shape)
+                results['From Lab'] = real
+                #for model in models:
+                model_pred, _ = model.test(self.x_test, self.y_real, metric)
+                results[model.get_name()] = np.squeeze(model_pred)
+                res = {}
+                res['From Lab'] = self.y_real
+                #for model in models:
+                model_pred, _ = model.test(self.x_test, self.y_real, metric)
+                res['Arrange']=np.squeeze(np.subtract(model_pred, res['From Lab']))
 
-        ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
-        ax1.xaxis.set_minor_locator(mdates.AutoDateLocator()) #Должен устанавливать неглавные вертикальные оси, но вот не надо это использовать, если слишком большой/неравномерный временной промежуток
-        ax1.xaxis.set_minor_formatter(mdates.DateFormatter(''))
-        ax1.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
-        ax1.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
-        ax1.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
-        ax1.xaxis.grid(True, which='major', linestyle='--', linewidth=1)
-        ax1.xaxis.grid(True, which='minor', linestyle=':', linewidth=0.8)
-        ax1.yaxis.set_label_coords(-0.1, 0.5)
-        ax1.xaxis.set_label_coords(1, -0.2)
+                #fig1, ax1 = plt.subplots()
+                #fig2, ax2 = plt.subplots()
+                #fig3, ax3 = plt.subplots()
+                #fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(8, 10))
+                ax1 = fig.add_subplot(grid[0, i])
+                ax2 = fig.add_subplot(grid[1, i])
+                ax3 = fig.add_subplot(grid[2, i])
+                for name, data in results.items():
+                    #print(data.shape)
+                    #print(type(data))
+                    ax1.plot(self.timestamps, data, linestyle='', marker=".", markersize=8, label=name)
+                    points_plot, = ax2.plot(res['From Lab'], res['Arrange'], linestyle='', marker='.')
+                    ax3.plot(results['From Lab'], results[model.get_name()], linestyle='', marker='.')
+                    #ax2.legend([points_plot], ['Points'])
+                    #ax2.plot()
+                    #print(data)
+                if lines:
+                    for i in range(len(self.timestamps)-1):
+                        ax1.plot([self.timestamps[i], self.timestamps[i+1]], [results['From Lab'][i], results['From Lab'][i+1]], color='blue',
+                                 linestyle='-', linewidth=0.6)
+                        ax1.plot([self.timestamps[i], self.timestamps[i + 1]],
+                                 [results[model.get_name()][i], results[model.get_name()][i + 1]], color='orange',
+                                 linestyle='-', linewidth=0.6)
+                if lines_vertical:
+                    zero=[0]*len(res['From Lab'])
+                    ideal_line, =ax2.plot(res['From Lab'], zero, linewidth=1.2, color='green')
+                    for i in range(len(self.timestamps)):
+                        ax2.plot([res['From Lab'][i], res['From Lab'][i]], [res['Arrange'][i], zero[i]], linewidth=0.7, color='red')
+                    ax2.legend([ideal_line], ['Ideal'], loc='best')
+                    #for i in range(len(self.timestamps)):
+                    #    ax1.plot([self.timestamps[i], self.timestamps[i]], [results['From Lab'][i], results[model.get_name()][i]], color='gray',
+                    #             linestyle='-')
+                x=np.linspace(min(real), max(real), 100)
+                y=x
+                ax3_ideal, =ax3.plot(x,y, linestyle='-', color='green', linewidth=0.6)
+                ax3.legend([ax3_ideal], ['Ideal'], loc='best')
+                # Устанавливаем параметры визуализации
+                ax1.set_title(self.name)
+                ax1.set_xlabel('Date')
+                ax1.set_ylabel('Count', rotation=90)
+                ax1.xaxis.set_major_formatter(mdates.DateFormatter('%Y-%m'))
+                ax1.xaxis.set_minor_formatter(mdates.AutoDateFormatter('%Y-%m'))
+                ax1.xaxis.set_tick_params(rotation=30)
+                ax1.legend(loc='best')
 
-        ax2.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
-        ax2.xaxis.set_minor_locator(ticker.AutoMinorLocator(2))
-        ax2.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
-        ax2.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
-        ax2.grid(True, axis='x', which='major', linestyle='--', linewidth=1)
-        ax2.grid(True, axis='x', which='minor', linestyle=':', linewidth=0.8)
-        ax2.yaxis.set_label_coords(-0.1, 0.5)
-        ax2.xaxis.set_label_coords(1, -0.2)
+                ax2.set_title("Model prediction errors")
+                ax2.set_xlabel('Real parameters')
+                ax2.set_ylabel('Error', rotation=90)
+                #ax2.set_xticks(rotation=45)
 
-        ax3.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
-        ax3.xaxis.set_minor_locator(ticker.AutoMinorLocator(2))
-        ax3.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
-        ax3.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
-        ax3.grid(True, axis='x', which='major', linestyle='--', linewidth=1)
-        ax3.grid(True, axis='x', which='minor', linestyle=':', linewidth=0.8)
-        ax3.yaxis.set_label_coords(-0.1, 0.5)
-        ax3.xaxis.set_label_coords(1, -0.2)
+                ax3.set_title('Real-predicted relation')
+                ax3.set_xlabel('Real')
+                ax3.set_ylabel('Predicted', rotation=90)
+                ax3.xaxis.set_tick_params(rotation=0)
+                #ax3.legend(loc='best')
 
-        plt.subplots_adjust(hspace=0.8)
-        #plt.tight_layout()
+                ax1.xaxis.set_major_locator(mdates.AutoDateLocator())
+                ax1.xaxis.set_minor_locator(mdates.AutoDateLocator()) #Должен устанавливать неглавные вертикальные оси, но вот не надо это использовать, если слишком большой/неравномерный временной промежуток
+                ax1.xaxis.set_minor_formatter(mdates.DateFormatter(''))
+                ax1.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+                ax1.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
+                ax1.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
+                ax1.xaxis.grid(True, which='major', linestyle='--', linewidth=1)
+                ax1.xaxis.grid(True, which='minor', linestyle=':', linewidth=0.8)
+                ax1.yaxis.set_label_coords(-0.1, 0.5)
+                ax1.xaxis.set_label_coords(1.05, -0.35)
+                ax1.xaxis.set_label_position('bottom')
+
+                ax2.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+                ax2.xaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+                ax2.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
+                ax2.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
+                ax2.grid(True, axis='x', which='major', linestyle='--', linewidth=1)
+                ax2.grid(True, axis='x', which='minor', linestyle=':', linewidth=0.8)
+                ax2.yaxis.set_label_coords(-0.1, 0.5)
+                ax2.xaxis.set_label_coords(1, -0.2)
+
+                ax3.yaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+                ax3.xaxis.set_minor_locator(ticker.AutoMinorLocator(2))
+                ax3.grid(True, axis='y', which='major', linestyle='-', linewidth=1)
+                ax3.grid(True, axis='y', which='minor', linestyle=':', linewidth=0.8)
+                ax3.grid(True, axis='x', which='major', linestyle='--', linewidth=1)
+                ax3.grid(True, axis='x', which='minor', linestyle=':', linewidth=0.8)
+                ax3.yaxis.set_label_coords(-0.1, 0.5)
+                ax3.xaxis.set_label_coords(1, -0.2)
+
+                plt.subplots_adjust(hspace=0.6)
+                #plt.tight_layout()
         plt.show()
         #print(results['From Lab'])
 
